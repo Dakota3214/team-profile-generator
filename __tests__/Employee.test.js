26464d42824965a2b35eb5e0906dffa29bb1c8ff
Dakota Donaldson <dakota.donaldson17@icloud.com>
2021-10-16 18:16:21 -0500

    Added create employee object function with passing test using jest

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,19 @@
-const checkIfEqual = require('../lib/Employee.js');
 
-test('checks if 10 is equal to 10', () => {
-  expect(checkIfEqual(10, 10)).toBe(true);
+const Employee = require('../lib/Employee.js');
+
+
+test('creates employee object', () => {
+  const employee = new Employee("Dakota", 1, "dakota.donaldson17@icloud.com");
+
+  expect(employee.name).toBe(expect.any(String));
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
+});
+
+test("gets employee's name value", () => {
+  const employee = new Employee("Dakota", 1, "dakota.donaldson17@icloud.com");
+
+  expect(employee.getName()).toBe(expect.stringContaining(employee.name));
+  //Current train of thoughts(1): getName() has to include the employee.name, how to connect these two?
+  //Once COT(1) is done, go back to COT(2) in Employee.js
 });
